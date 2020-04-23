9dfbb706ac4c88c13b6dcdc3c5012807d61fdae3
horikawa1218 <daichi.horikawa@force-corp.co.jp>
2020-04-23 18:34:41 +0900

    javascript

---
+++ js/jspractice.js
@@ -0,0 +1,35 @@
+// document.write("Hello World!");
+// alert("Hello World!");
+
+// console.log("Hello World" + "田中さん");
+// console.log(10 + 10);
+// console.log("10" + "10");
+// console.log(1 - 1);
+// console.log(2 * 2);
+// console.log(10 / 3);
+
+// let x = 1;
+// let y = 1;
+// x++;
+// x++;
+// console.log(x);
+// y--;
+// console.log(y);
+
+let x = 7;
+let y = x++;
+let a = 7;
+let b = ++a;
+
+console.log(x);
+console.log(y);
+console.log(a);
+console.log(b);
+
+//変数とは、値を代入するための箱
+//定数とは、値を別名で定義したもの
+
+let age = 20;
+const name = "田中ヒメ";
+
+console.log(`${name}さんの年齢は${age}です`);
